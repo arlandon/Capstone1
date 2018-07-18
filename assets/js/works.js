d57b5616452f9de502d716ffa4a24744fc8ed830
arlandon <arlandon.regero@gmail.com>
2018-07-19 00:23:52 +0800

    updating works page

---
+++ assets/js/works.js
@@ -0,0 +1,82 @@
+function myFunctionEvent() {
+    var x = document.getElementById("divEvent");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+
+function myFunctionBrand() {
+    var x = document.getElementById("divBrand");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunctionCreative() {
+    var x = document.getElementById("divCreative");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunctionFab() {
+    var x = document.getElementById("divFab");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunctionPrint() {
+    var x = document.getElementById("divPrint");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunctionCon() {
+    var x = document.getElementById("divCon");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunctionCor() {
+    var x = document.getElementById("divCor");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunctionMan() {
+    var x = document.getElementById("divMan");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunctionSell() {
+    var x = document.getElementById("divSell");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
